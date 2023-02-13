daa29e82244b6d2c609dcf86b79f501e3260ac1b
Igor-0279 <115004091+Igor-0279@users.noreply.github.com>
2023-02-13 08:51:54 +0200

    6task,-10task

--- js/task-06.js
+++ js/task-06.js
@@ -7,14 +7,26 @@
 // Для додавання стилів використовуй CSS-класи valid і invalid,
 //  які ми вже додали у вихідні файли завдання.
 
-const inputElem = document.querySelector('input');
-// console.log(inputElem.dataset.length);
+// const inputElem = document.querySelector('input');
 
-const borderRemove = (event) => {
- let inputText = event.currentTarget.value;
+// const borderRemove = (event) => {
+//  let inputText = event.currentTarget.value;
   
- inputText.length !== Number(inputElem.dataset.length)
-   ? inputElem.classList.add("invalid")
-   : inputElem.classList.remove("invalid") || inputElem.classList.add('valid');
-}
-inputElem.addEventListener("blur", borderRemove);
+//  inputText.length !== Number(inputElem.dataset.length)
+//    ? inputElem.classList.add("invalid")
+//    : inputElem.classList.remove("invalid") || inputElem.classList.add('valid');
+// }
+// inputElem.addEventListener("blur", borderRemove);
+
+
+const inputElem = document.querySelector('input');
+
+inputElem.addEventListener("blur", function () {
+  if (inputElem.value.length !== Number(inputElem.dataset.length)) {
+    inputElem.classList.add("invalid");
+    inputElem.classList.remove("valid");
+  } else {
+    inputElem.classList.add("valid");
+    inputElem.classList.remove("invalid");
+  }
+});
